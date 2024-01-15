b9ecbaa6a0ea59a85edd2907d34a1b9f25c76e33
mhabbatein <habiburrahmanmuhammad95@gmail.com>
2024-01-15 22:17:32 +0700

    add text habib

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      Data Management System Barzadata
+      Data Management System Barzadata (habib)
     </main>
   );
 }
