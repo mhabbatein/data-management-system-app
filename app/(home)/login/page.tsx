e425dc3b12e20193cde515a6e30fcfad7f313748
mhabbatein <habiburrahmanmuhammad95@gmail.com>
2024-01-15 23:46:24 +0700

    add login box

--- app/(home)/login/page.tsx
+++ app/(home)/login/page.tsx
@@ -1,5 +1,17 @@
+import { LoginForm } from "@/components/LoginForm";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import React from "react";
 
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      {/* Login Box */}
+      <section className="flex flex-col items-center gap-6 rounded-lg border border-input p-4">
+        <LoginForm />
+        <p className="text-sm">Atau</p>
+        <Button className="w-full">Login melalui Google</Button>
+      </section>
+    </div>
+  );
 }
