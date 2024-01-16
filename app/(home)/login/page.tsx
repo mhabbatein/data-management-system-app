7463acb0860449b1b814b6ec6c92dbe561bb3bf5
mhabbatein <habiburrahmanmuhammad95@gmail.com>
2024-01-16 19:52:15 +0700

    refactor login page

--- app/(home)/login/page.tsx
+++ app/(home)/login/page.tsx
@@ -6,12 +6,7 @@ import React from "react";
 export default function LoginPage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      {/* Login Box */}
-      <section className="flex flex-col items-center gap-6 rounded-lg border border-input p-4">
-        <LoginForm />
-        <p className="text-sm">Atau</p>
-        <Button className="w-full">Login melalui Google</Button>
-      </section>
+      <LoginForm />
     </div>
   );
 }
