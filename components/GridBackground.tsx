8983f5b42b29f3f9a60be95b1db7b7ea7968ce07
mhabbatein <habiburrahmanmuhammad95@gmail.com>
2024-01-17 17:57:40 +0700

    add GridBackground

---
+++ components/GridBackground.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function GridBackground({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex  h-[50rem] w-full items-center justify-center bg-white dark:bg-black">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+      {children}
+    </div>
+  );
+}
