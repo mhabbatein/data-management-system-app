b4143f4a96a326a252faae747a408778950a0c09
mhabbatein <habiburrahmanmuhammad95@gmail.com>
2024-01-18 19:43:23 +0700

    fix GridBackground responsivity

--- components/GridBackground.tsx
+++ components/GridBackground.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function GridBackground({ children }: { children: React.ReactNode }) {
   return (
-    <div className="dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex  h-[50rem] w-full items-center justify-center bg-white dark:bg-black">
+    <div className="relative flex h-[calc(100vh-120px)] w-full items-center justify-center bg-white bg-grid-black/[0.2] dark:bg-black dark:bg-grid-white/[0.2] sm:h-screen">
       {/* Radial gradient for the container to give a faded look */}
       <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
       {children}
