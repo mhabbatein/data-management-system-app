151548a999ff7e44b6764d625c83601904c5df09
mhabbatein <habiburrahmanmuhammad95@gmail.com>
2024-01-19 21:35:00 +0700

    add table example

--- app/filter/peraih-beasiswa/page.tsx
+++ app/filter/peraih-beasiswa/page.tsx
@@ -1,5 +1,63 @@
-import React from "react";
+import { promises as fs } from "fs";
+import path from "path";
+import { Metadata } from "next";
+import Image from "next/image";
+import { z } from "zod";
 
-export default function PeraihBeasiswaPage() {
-  return <div className='flex min-h-[calc(100vh-72px)] flex-col items-center justify-center md:min-h-screen'>PeraihBeasiswaPage</div>;
+import { columns } from "@/components/data-table/columns";
+import { DataTable } from "@/components/data-table/data-table";
+import { UserNav } from "@/components/data-table/user-nav";
+import { taskSchema } from "@/data/schema";
+
+export const metadata: Metadata = {
+  title: "Tasks",
+  description: "A task and issue tracker build using Tanstack Table.",
+};
+
+// Simulate a database read for tasks.
+async function getTasks() {
+  const data = await fs.readFile(path.join(process.cwd(), "/data/tasks.json"));
+
+  const tasks = JSON.parse(data.toString());
+
+  return z.array(taskSchema).parse(tasks);
+}
+
+export default async function PeraihBeasiswaPage() {
+  const tasks = await getTasks();
+
+  return (
+    <>
+      <div className="md:hidden">
+        <Image
+          src="/examples/tasks-light.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="block dark:hidden"
+        />
+        <Image
+          src="/examples/tasks-dark.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <p className="text-muted-foreground">
+              Here&apos;s a list of your tasks for this month!
+            </p>
+          </div>
+          <div className="flex items-center space-x-2">
+            <UserNav />
+          </div>
+        </div>
+        <DataTable data={tasks} columns={columns} />
+      </div>
+    </>
+  );
 }
