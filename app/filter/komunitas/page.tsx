9c11e913011d0bdf7caef81286954922dcba684a
mhabbatein <habiburrahmanmuhammad95@gmail.com>
2024-02-07 14:37:42 +0700

    fix page size

--- app/filter/komunitas/page.tsx
+++ app/filter/komunitas/page.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function KomunitasPage() {
   return (
-    <div>KomunitasPage</div>
-  )
+    <div className="flex min-h-[calc(100vh-72px)] flex-col items-center justify-center md:min-h-screen">
+      KomunitasPage
+    </div>
+  );
 }
